9cc4b7158439339099e21e31999be6cd83fd7127
Metehan Kozan <metehankozan@hotmail.com.tr>
2021-07-19 20:21:57 +0300

    first commit

---
+++ Odev-12/Odev12.sql
@@ -0,0 +1,20 @@
+-- 1. film tablosunda film uzunluğu length sütununda gösterilmektedir. Uzunluğu ortalama film uzunluğundan fazla kaç tane film vardır?
+SELECT COUNT(*) FROM film
+WHERE length > (SELECT AVG(length) FROM film);
+
+-- 2. film tablosunda en yüksek rental_rate değerine sahip kaç tane film vardır?
+SELECT COUNT(*) FROM film
+WHERE rental_rate = (SELECT MAX(rental_rate) FROM film);
+
+-- 3. film tablosunda en düşük rental_rate ve en düşük replacement_cost değerlerine sahip filmleri sıralayınız.
+SELECT * FROM film
+WHERE rental_rate = (SELECT MIN(rental_rate) FROM film) AND 
+	replacement_cost = (SELECT MIN(replacement_cost) FROM film);
+
+-- 4. payment tablosunda en fazla sayıda alışveriş yapan müşterileri(customer) sıralayınız.
+SELECT customer.customer_id, customer.first_name, customer.last_name, SubQ.count 
+FROM customer
+INNER JOIN (SELECT customer_id, COUNT(*) AS count FROM payment
+			GROUP BY customer_id) AS SubQ 
+		ON customer.customer_id = SubQ.customer_id
+ORDER BY count DESC;
